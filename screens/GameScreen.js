3b95878b0f8b3cd652517569d79f9d4dc120d73a
Bilot Xamarin development <bilotdevappleid@bilot.fi>
2019-12-02 11:40:17 +0200

    Adding Dimensions when calculating width

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,7 +6,8 @@ import {
   StyleSheet,
   Alert,
   FlatList,
-  ScrollView
+  ScrollView,
+  Dimensions
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import NumberContainer from "./../components/NumberContainer";
@@ -71,6 +72,10 @@ const GameScreen = props => {
     setPastGuesses(curRounds => [nextNumber.toString(), ...curRounds]);
   };
 
+  let listContainerStyle = styles.listContainer
+
+  if(Dimensions.get('window').width < 350) listContainerStyle = styles.listContainerBig
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guest</Text>
@@ -117,13 +122,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20: 5,
     width: 400,
     maxWidth: "90%"
   },
   listContainer: {
     width: "60%",
     flex: 1
+  },
+  listContainerBig: {
+    flex: 1,
+    width: '80%',
   },
   list: {
     flexGrow: 1,
